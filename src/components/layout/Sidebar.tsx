212fc1ec33241b29044a65e1b77123c9b782e3c2
Kaleem Hassan <kaleemhassanansari@gmail.com>
2026-05-13 16:29:13 +0500

    import

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "react-Link";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { useAppSelector } from "@/store/hooks";
